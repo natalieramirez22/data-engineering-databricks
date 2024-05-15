770cd4fabd455b6bd9fd79ba45275722e3121c0a
natalieramirez22 <natalieramirez22>
2024-05-15 15:22:33 +0000

    Added New Example

---
+++ Example/Untitled Notebook 2024-05-15 11_19_52.py
@@ -0,0 +1,14 @@
+# Databricks notebook source
+print("First notebook")
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC select "natalie ramirez" as name,
+# MAGIC        "19" as age,
+# MAGIC        "p&g" as company,
+# MAGIC        "data asset management intern" as title
+
+# COMMAND ----------
+
+
